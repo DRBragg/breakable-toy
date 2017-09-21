948790641354a7e301476e1454259f172e078c02
Drew Bragg <DRBragg@gmail.com>
2017-09-21 16:54:54 -0400

    created comments table. created comments model. made proper associations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170921165258) do
+ActiveRecord::Schema.define(version: 20170921193419) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,13 @@ ActiveRecord::Schema.define(version: 20170921165258) do
     t.string "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "cards_decks", id: false, force: :cascade do |t|
+    t.bigint "card_id", null: false
+    t.bigint "deck_id", null: false
+    t.index ["card_id", "deck_id"], name: "index_cards_decks_on_card_id_and_deck_id"
+    t.index ["deck_id", "card_id"], name: "index_cards_decks_on_deck_id_and_card_id"
   end
 
   create_table "comments", force: :cascade do |t|
